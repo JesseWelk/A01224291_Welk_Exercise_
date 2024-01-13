c5b12b3df197775016b746ff0d104eae8ddbddfe
JesseWelk <jwelk@my.bcit.ca>
2024-01-13 13:29:45 -0800

    final clean

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import dynamic from "next/dynamic"
 
-const DynamicMap = dynamic(() => import('../../components/Map'), {ssr: false});
+const DynamicMap = dynamic(() => import('../../components/Map'), { ssr: false });
 
 export default function Home() {
   return (
     <div>
-    <h1 className="mt-5 flex justify-center items-center">
-      <img src="images/Rail Logo.png" alt="Welk Railway" />
-    </h1>
-    <DynamicMap/>
+      <h1 className="mt-5 flex justify-center items-center">
+        <img src="images/Rail Logo.png" alt="Welk Railway" />
+      </h1>
+      <DynamicMap />
     </div>
   )
 }
